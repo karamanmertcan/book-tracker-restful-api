492b289ce2eb587516b183e40118893baa65abfc
karamanmertcan <20181701056@stu.khas.edu.tr>
2022-01-09 01:15:17 +0300

    quote feature added

--- controllers/quote.js
+++ controllers/quote.js
@@ -17,3 +17,47 @@ export const getQuotes = async (req, res) => {
     });
   }
 };
+
+export const addQuotes = async (req, res) => {
+  const { quote, bookName } = req.body;
+  console.log(req.body);
+  try {
+    if (!quote) return res.status(400).json({ error: 'Alıntı Girilmedi !!!' });
+    if (!bookName) return res.status(400).json({ error: 'Kitap Adı Girilmedi !!!' });
+
+    const quoteObj = await Quote.create({
+      quote,
+      bookOwnerId: req.user._id,
+      bookName,
+      bookId: req.body.bookId
+    });
+
+    return res.status(200).json({
+      ok: true,
+      message: 'Alıntı Başarıyla Eklendi !!!',
+      quote: quoteObj
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({ error: 'Alıntı Eklenemedi !!!' });
+  }
+};
+
+export const getSingleBookQuotes = async (req, res) => {
+  console.log(req.params);
+  try {
+    const quotes = await Quote.find({ bookId: req.params.id });
+
+    if (!quotes) return res.status(400).json({ error: 'Alıntılar Getirilimedi !!!' });
+
+    return res.status(200).json({
+      ok: true,
+      quotes
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({
+      error: 'Alıntılar Getirilimedi !!!'
+    });
+  }
+};
--- routes/quote.js
+++ routes/quote.js
@@ -1,9 +1,12 @@
 import express from 'express';
-import { getQuotes } from '../controllers/quote';
+import { addQuotes, getQuotes, getSingleBookQuotes } from '../controllers/quote';
 import { requireSignin } from '../middlewares/auth';
 
 const router = express.Router();
 
 router.get('/get-user-quotes', requireSignin, getQuotes);
+router.get('/get-single-book-quotes/:id', requireSignin, getSingleBookQuotes);
+
+router.post('/add-user-quotes', requireSignin, addQuotes);
 
 module.exports = router;
